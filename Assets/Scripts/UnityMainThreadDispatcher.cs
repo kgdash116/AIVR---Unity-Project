7c60fd4702832a8eb389aa85e1aff5f27e24e72a
Waqas Kureshy <waqaskureshy@Waqass-MacBook-Pro.local>
2023-11-30 02:03:10 -0800

    Added Main Thread Dispatcher

---
+++ Assets/Scripts/UnityMainThreadDispatcher.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UnityMainThreadDispatcher : MonoBehaviour
+{
+    private static UnityMainThreadDispatcher _instance;
+    private static readonly Queue<Action> _executionQueue = new Queue<Action>();
+
+    public static UnityMainThreadDispatcher Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                GameObject go = new GameObject("UnityMainThreadDispatcher");
+                _instance = go.AddComponent<UnityMainThreadDispatcher>();
+            }
+            return _instance;
+        }
+    }
+
+    private void Update()
+    {
+        lock (_executionQueue)
+        {
+            while (_executionQueue.Count > 0)
+            {
+                _executionQueue.Dequeue().Invoke();
+            }
+        }
+    }
+
+    public static void Enqueue(Action action)
+    {
+        lock (_executionQueue)
+        {
+            _executionQueue.Enqueue(action);
+        }
+    }
+}
